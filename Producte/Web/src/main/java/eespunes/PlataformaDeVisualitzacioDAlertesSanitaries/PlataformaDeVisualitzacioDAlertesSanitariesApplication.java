89ad0b8114e46e9b536ba76916d01dc82b4eaa8f
eespunes <erik.espunyes7@outlook.com>
2021-01-01 10:06:57 +0100

    S'han creat els projectes
    Signed-off-by: eespunes <erik.espunyes7@outlook.com>

---
+++ Producte/Web/src/main/java/eespunes/PlataformaDeVisualitzacioDAlertesSanitaries/PlataformaDeVisualitzacioDAlertesSanitariesApplication.java
@@ -0,0 +1,13 @@
+package eespunes.PlataformaDeVisualitzacioDAlertesSanitaries;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlataformaDeVisualitzacioDAlertesSanitariesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlataformaDeVisualitzacioDAlertesSanitariesApplication.class, args);
+	}
+
+}
---
+++ Producte/Web/src/test/java/eespunes/PlataformaDeVisualitzacioDAlertesSanitaries/AlertesSanitariesApplicationTests.java
@@ -0,0 +1,13 @@
+package eespunes.AlertesSanitaries;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlertesSanitariesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
