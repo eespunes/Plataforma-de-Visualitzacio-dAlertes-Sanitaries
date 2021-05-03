b019c45054aa92e4a3bf15993df4bda07a125aea
eespunes <erik.espunyes7@outlook.com>
2021-05-03 12:32:13 +0200

    Project Updated

---
+++ Producte/App/src/Scenes/ListScene.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Text, StyleSheet } from "react-native";
+
+const ListScene = () => {
+  return <Text style={styles.text}>List Screen</Text>;
+};
+
+const styles = StyleSheet.create({
+  text: {
+    fontSize: 50
+  }
+});
+
+export default ListScene;
