1093f402387dd228e1f8e90244e88784c00aea5e
eespunes <erik.espunyes7@outlook.com>
2021-06-11 09:32:42 +0200

    Minor bug

--- Producte/App/src/domain/CommunicationManager.js
+++ Producte/App/src/domain/CommunicationManager.js
@@ -7,20 +7,25 @@ export default class savedData {
     static fromNotification;
 
     static checkWarningColor = function (warning) {
-        if (warning !== undefined) return 0
+        if (warning === undefined) return 0
+
         if (warning.greenValue < warning.yellowValue) {
-            if (warning.lastValue <= warning.greenValue)
+            if (warning.lastValue <= warning.greenValue) {
                 return 0
-            else if (warning.lastValue <= warning.redValue)
+            }
+            else if (warning.lastValue <= warning.redValue) {
                 return 1
+            }
             else {
                 return 2
             }
         } else {
-            if (warning.lastValue >= warning.greenValue)
+            if (warning.lastValue >= warning.greenValue) {
                 return 0
-            else if (warning.lastValue >= warning.redValue)
+            }
+            else if (warning.lastValue >= warning.redValue) {
                 return 1
+            }
             else {
                 return 2
             }
